c7d408fe122bac91bd9a4f32074b099eecf753cc
sixiyuanzi <1911474072@qq.com>
2022-08-06 15:51:29 +0800

    完成率主页功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -4,5 +4,25 @@ $.ajaxPrefilter(option => {
   // 在发起ajax请求时统一路径
   option.url = 'http://www.liulongbin.top:3007' + option.url
 
-  console.log(option.url)
+  // console.log(option.url)
+  // 给包含my路径的地址添加同意请求头
+  // indexOf == -1 则表示不包含
+  if(option.url.indexOf('/my/') !== -1) {
+    option.headers = {
+      Authorization : localStorage.getItem('token') || ''
+    }
+  }
+
+  //不管是否获取成功都会调用complete这个回调函数
+  option.complete = function(res){
+    console.log(res)
+    const {status,message} = res.responseJSON
+    // 判断res.responJSON
+    if(status === 1 && message === '身份认证失败！'){
+      // 1.强制清空本地token
+      localStorage.removeItem('token')
+      // 2.强制跳转页面到login.html
+      location.href = '/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,73 @@
+$(() => {
+  // 调用数据
+  getData()
+
+  const layer = layui.layer
+  // 退出功能
+  $(`#btnLoginout`).on(`click` , function() {
+    // 退出询问
+    layer.confirm('确定退出？', {icon: 3, title:'提示'}, function(index){
+      //do something
+      // 1.清空本地存储的token
+      localStorage.removeItem('token')
+      // 2.跳转到登录页面
+      location.href = './login.html'
+      
+      layer.close(index);
+    });
+  })
+
+
+})
+  // 封装获取用户基本信息函数
+  function getData(){
+    // 发起ajax请求
+    $.ajax({
+      type: "GET",
+      url: '/my/userinfo',
+      // 设置请求头部信息
+      // headers:{
+      //   Authorization : localStorage.getItem('token') || ''
+      // } ,
+      success: function (res) {
+        // console.log(res)
+
+        // 解构对象
+        const {status , data} =res
+
+        if(status !== 0 ) return layui.layer.msg(`获取用户信息失败！`)
+
+        // 渲染头像函数
+        renderAvatar(data)
+      },
+      
+    });
+
+    function renderAvatar(data){
+      // 用户有昵称先出现昵称，没有就出现a
+      const name = data.nickname ? data.nickname : data.username
+
+      $(`.welcome`).html(`欢迎&nbsp;&nbsp;${name}`)
+
+      // 头像图片
+      if(data.user_pic !== null) {
+        $('.layui-nav-img').attr('str' ,data.user_pic).show()
+        $(`.text-avatar`).hide()
+      } else {
+        const first = name[0]
+        $(`.text-avatar`).html(first).show()
+        $('.layui-nav-img').hide()
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
